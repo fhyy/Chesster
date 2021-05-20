b2e907f36c42cf93600823f272c6c8e17109ca3b
Marcus <marcus.lignercrona@gmail.com>
2021-05-20 19:49:25 +0200

    Added chesspieces unicode to assets

--- assets.py
+++ assets.py
@@ -1,1 +1,13 @@
 #Asset file for Chesster
+import discord
+    var whiteKing = "\u2664"
+    var whiteQueen = "\u2665"
+    var whiteBishop = "\u2665"
+    var whiteKnight = "\u2665"
+    var whitePawn = "\u2665"
+    var blackKing = "\u2664"
+    var blackQueen = "\u2665"
+    var blackBishop = "\u2665"
+    var blackKnight = "\u2665"
+    var blackPawn = "\u2665"
+   
