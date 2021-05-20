c6c6e387ad3e5d747909c94d3479ac0360daefa6
fhyy <fhyy@kth.se>
2021-05-20 23:14:26 +0200

    Added an ugly way to print a board, just to try that the assets work and looks good

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import ids
 import setup
+import assets as ass
 
 intents = discord.Intents(messages=True, members=True, guilds=True)
 client = discord.Client(intents=intents)
@@ -22,5 +23,21 @@ async def on_message(message):
     
     if message.content == "new game":
         await client.get_channel(channelId).send(resetBoard())
+        
+    if message.content == "Print board":
+        await client.get_channel(channelId).send(getBoardInUglyManner())
 
+def getBoardInUglyManner():
+    return (
+        " ABCDEFGH\n"
+        "8"+ ass.blackRook + ass.blackKnight + ass.blackBishop + ass.blackQueen + ass.blackKing + ass.blackBishop + ass.blackKing + ass.blackRook + '\n'
+        "7"+ ass.blackPawn + ass.blackPawn + ass.blackPawn + ass.blackPawn + ass.blackPawn + ass.blackPawn + ass.blackPawn + ass.blackPawn + '\n'
+        "6"+ ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + '\n'
+        "5"+ ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + '\n'
+        "4"+ ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + '\n'
+        "3"+ ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + ass.blackSquare + ass.whiteSquare + '\n'
+        "2"+ ass.whitePawn + ass.whitePawn + ass.whitePawn + ass.whitePawn + ass.whitePawn + ass.whitePawn + ass.whitePawn + ass.whitePawn + '\n'
+        "1"+ ass.whiteRook + ass.whiteKnight + ass.whiteBishop + ass.whiteQueen + ass.whiteKing + ass.whiteBishop + ass.whiteKing + ass.whiteRook + '\n'
+    )
+        
 client.run(clientKey)
