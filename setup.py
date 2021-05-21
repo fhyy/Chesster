d83c94fa2956c3977d88cf0aaf06993530a52b54
Fredrik Hyyrynen <fredrik.hyyrynen@gmail.com>
2021-05-21 23:32:01 +0200

    Fixed setup as checkerboard was deleted

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import assets
 def resetBoard():  
     string = ""
     for x in range(4):
-        string += assets.checkerBoard + assets.checkerBoard +  assets.checkerBoard + assets.checkerBoard + "\n"
+        string += assets.blackSquare + assets.whiteSquare +  assets.blackSquare + assets.whiteSquare +  assets.blackSquare + assets.whiteSquare +  assets.blackSquare + assets.whiteSquare  + "\n" +
+        assets.whiteSquare +  assets.blackSquare + assets.whiteSquare +  assets.blackSquare + assets.whiteSquare +  assets.blackSquare + assets.whiteSquare  + assets.blackSquare
     return string
 
 print(resetBoard())
